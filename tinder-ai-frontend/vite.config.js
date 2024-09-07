f24e6451d5c2b5af8e8519f6d19d34139500daf6
Hmtgit7 <hmtloharcoding3579@gmail.com>
2024-09-07 16:54:43 +0530

    add tailwindcss and remove existing code

--- tinder-ai-frontend/vite.config.js
+++ tinder-ai-frontend/vite.config.js
@@ -6,9 +6,9 @@ import tailwindcss from 'tailwindcss'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css:{
-    postcss:{
-        plugins:[tailwindcss()]
+  css: {
+    postcss: {
+      plugins: [tailwindcss()]
     }
   }
 })
